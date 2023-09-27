c3524ebafb87c2c04001f1d5b610c1709b5301b8
naseershaik456 <131879703+naseershaik456@users.noreply.github.com>
2023-09-27 12:45:26 +0530

    app.py

--- app.py
+++ app.py
@@ -4,10 +4,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-# Store admission data in-memory (not recommended for production)
 admission_data = []
 
-# API endpoint to submit admission form
 @app.route('/api/admission', methods=['POST'])
 def submit_admission_form():
     data = request.json
@@ -15,7 +13,6 @@ def submit_admission_form():
     
     return jsonify({'message': 'Admission form submitted successfully'})
 
-# API endpoint to retrieve all admission data
 @app.route('/api/admissions', methods=['GET'])
 def get_admission_data():
     if request.method == "GET":
